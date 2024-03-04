148d08c87af62fe4b2bb53d927d5c190fab16ea0
xd21736 <98823093+xd21736@users.noreply.github.com>
2024-03-04 13:34:52 +0000

    Basic Natural Parameter
    Currently only for cubic equation

--- Week17Continuation.py
+++ Week17Continuation.py
@@ -5,18 +5,28 @@ Created on Mon Feb 26 15:30:33 2024
 @author: farka
 """
 
-import CommonModules
+from CommonModules import *
 import Week16General
 import Week17Functions
     
 def NaturalParameterContintuation(Func,X0,Param0,ParamNSteps,ParamStep=0.1):#,discretisation=lambda x:x):
     ParameterSpace =np.linspace(Param0,Param0+ParamStep*ParamNSteps,ParamNSteps)
-    SolutionSpace = np.zeros((size(X0),ParamNSteps))
-    SolutionSpace[0,:] = X0
+    SolutionSpace = np.zeros([ParamNSteps,(np.size(X0))])
+    SolutionSpace[0,:] = scipy.optimize.root(lambda x: Func(x,1,Param0),X0).x
     for i in range(1,ParamNSteps):
         CurrentParam = ParameterSpace[i]
-        SolutionSpace[i,:] = scipy.optimize.root(lambda x: Func(x,CurrentParam),SolutionSpace[i:-1])
+        #print(CurrentParam)
+        Guess = scipy.optimize.root(lambda x: Func(x,1,CurrentParam),SolutionSpace[i-1,:])
+        #print(Guess.success)
+        SolutionSpace[i,:] = Guess.x
+    return SolutionSpace, ParameterSpace 
+    
         
-    
-    
-    
+def Main():
+    Answer = NaturalParameterContintuation(Week17Functions.Cubic, 1.1, -2, 40) 
+    return Answer       
+
+if __name__ == "__main__":
+    Answer,Space = Main()
+    for i in range(len(Answer)):
+        plt.scatter(Space[i],Week17Functions.Cubic(Answer[i], 0,Space[i]))
